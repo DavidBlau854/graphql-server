5d39493f96fe96075c93bf1b4a649ba3202e98cc
daveBlue854 <dudiblau11@gmail.com>
2020-09-03 14:47:54 +0300

    some more

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
-    info: String
+    users: [User!]!
+    user(id:ID!): User
+}
+
+type Mutations { 
+    createUer(name:String!):User!
+}
+
+type User {
+    id: ID!
+    name : String!
 }
 `
 
 const resolvers = {
-    Query: { info: () => `this is the API of hackernews clone` }
+    Query: { info: () => null }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
