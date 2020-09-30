bf1d4b85112ee25403e79f90057bbb0a97149414
daveBlue854 <dudiblau11@gmail.com>
2020-09-30 15:57:34 +0300

    adding prisma 2

--- src/index.js
+++ src/index.js
@@ -3,51 +3,22 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-let links = [
-    { id: 'link-0', url: 'www.howto.com', description: 'this is a nice site' },
-    { id: 'link-1', url: 'www.google.com', description: 'this site is used for searching' }
-
-]
-
-let idCount = links.length
-
 const resolvers = {
     Query: {
         info: () => 'this is the API of Hackernews',
-        feed: () => links,
-        getLink: (parent, args) => links.find(link => link.id == `link-${args.id}`)
+        feed: (async (parent, args, context) => context.prisma.link.findMany()),
     },
     Mutation: {
-        delete: (parent, args) => {
-            const id = args.id
-            const linkIndx = links.findIndex(link => link.id == `link-${id}`)
-            const deletedLink = links[linkIndx]
-            if (linkIndx != -1) {
-                links.splice(linkIndx, 1)
-            }
-            return deletedLink
-        },
-        put: (parent, args) => {
-            const id = args.id
-            const linkIndx = links.findIndex(link => link.id == `link-${id}`)
-
-            links[linkIndx].url = args.url || links[linkIndx].url
-            links[linkIndx].description = args.description || links[linkIndx].description
-            return links[linkIndx]
-        },
-
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
+        post: (parent, args, context, info) => {
+            const link = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                }
+            })
             return link
         }
     },
-
-
 }
 
 const server = new GraphQLServer({ typeDefs: './src/schema.graphql', resolvers, context: { prisma } })
