cc3f92534d503a3f40fa1fb10c35c22475f6bd29
daveBlue854 <dudiblau11@gmail.com>
2020-10-06 15:32:39 +0300

    added filter to querying feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,9 +2,24 @@ function info() {
     return 'this is the API of Hackernews'
 
 }
-
+/**
+ * notice that filter passed in args.filter are only checked against desc or url on the links
+ * 
+ */
 async function feed(parent, args, context) {
-    return context.prisma.link.findMany()
+    const searchString = args.filter
+    let query = {}
+
+    if (searchString) {
+        query = {
+            OR: [
+                { description: { contains: searchString } },
+                { url: { contains: searchString } },
+            ]
+        }
+    }
+
+    return context.prisma.link.findMany({ where: query })
 }
 
 module.exports = {
