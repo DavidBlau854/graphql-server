8f08e7fa111ef47a12de8e88d6774b022154b23f
daveBlue854 <dudiblau11@gmail.com>
2020-10-06 15:19:36 +0300

    adding voting on links

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -23,7 +23,6 @@ async function signup(parent, args, context, info) {
     const password = await bcrypt.hash(args.password, 10)
     const user = await context.prisma.user.create({ data: { ...args, password } })
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
-    // return { token, user }
     return new AuthPayload(token, user)
 
 }
@@ -34,8 +33,36 @@ async function login(parent, args, context, info) {
     const valid = await bcrypt.compare(args.password, user.password)
     if (!valid) throw new Error("password not valid")
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
-    // return { token, user }
     return new AuthPayload(token, user)
+}
+
+async function vote(parent, args, context, info) {
+    const userId = getUserId(context)
+
+    throwIfVoteExists(args.linkId, userId)
+
+    const newVote = context.prisma.vote.create({
+        data: {
+            user: { connect: { id: userId } },
+            link: { connect: { id: Number(args.linkId) } }
+        }
+    })
+    context.pubsub.publish("NEW_VOTE", newVote)
+    return newVote
+}
+
+
+async function throwIfVoteExists(linkId, userId) {
+    const vote = await context.prisma.vote.findOne({
+        where: {
+            linkId_userId: {
+                linkId: Number(linkId),
+                userId: userId
+            }
+        }
+    })
+
+    if (Boolean(vote)) throw new Error('Already voted for that link', linkId)
 }
 
 class AuthPayload {
@@ -49,5 +76,6 @@ class AuthPayload {
 module.exports = {
     signup,
     login,
-    post
+    post,
+    vote
 }
